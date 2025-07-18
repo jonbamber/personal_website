ee9cee0e05a52c2f15cb9180dacb9031432262f5
Jon Bamber <jonbamber@hotmail.com>
2025-07-18 20:57:37 +0100

    troubleshooting

--- terraform/website/s3.tf
+++ terraform/website/s3.tf
@@ -68,10 +68,10 @@ resource "aws_s3_bucket_policy" "website" {
 resource "aws_s3_bucket_public_access_block" "my_bucket_public_access_block" {
   bucket = aws_s3_bucket.website.id
 
-  block_public_acls       = true
-  ignore_public_acls      = true
-  block_public_policy     = true
-  restrict_public_buckets = true
+  block_public_acls       = false
+  ignore_public_acls      = false
+  block_public_policy     = false
+  restrict_public_buckets = false
 }
 
 resource "aws_s3_bucket_object" "index_document" {
