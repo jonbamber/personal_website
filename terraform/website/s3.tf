02b6e1e894783446f19a0fb87d007e52df22cee5
Jon Bamber <jonbamber@hotmail.com>
2025-07-18 23:19:41 +0100

    troubleshooting

--- terraform/website/s3.tf
+++ terraform/website/s3.tf
@@ -52,11 +52,11 @@ data "aws_iam_policy_document" "website" {
       identifiers = ["*"]
     }
 
-    condition {
-      test     = "Bool"
-      variable = "aws:SecureTransport"
-      values   = ["false"]
-    }
+    # condition {
+    #   test     = "Bool"
+    #   variable = "aws:SecureTransport"
+    #   values   = ["false"]
+    # }
   }
 }
 
@@ -68,10 +68,10 @@ resource "aws_s3_bucket_policy" "website" {
 resource "aws_s3_bucket_public_access_block" "my_bucket_public_access_block" {
   bucket = aws_s3_bucket.website.id
 
-  block_public_acls       = false
-  ignore_public_acls      = false
-  block_public_policy     = false
-  restrict_public_buckets = false
+  block_public_acls       = true
+  ignore_public_acls      = true
+  block_public_policy     = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_object" "index_document" {
