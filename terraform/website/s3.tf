ce2989505aa63a0120cdbf849a1c00df6ef8593d
Jon Bamber <jonbamber@hotmail.com>
2025-07-18 22:34:30 +0100

    troubleshooting

--- terraform/website/s3.tf
+++ terraform/website/s3.tf
@@ -30,8 +30,8 @@ data "aws_iam_policy_document" "website" {
     resources = ["${aws_s3_bucket.website.arn}/*"]
 
     principals {
-      identifiers = ["*"]
       type        = "AWS"
+      identifiers = [aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn]
     }
 
     condition {
